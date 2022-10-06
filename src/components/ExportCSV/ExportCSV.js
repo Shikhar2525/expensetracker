149fe553ebbb8200a804e5e378398a1aa0c9a525
Shikhar Mandloi <shikhar.mandloi@gmai.com>
2022-10-06 23:44:52 +0530

    report button css issue

--- src/components/ExportCSV/ExportCSV.js
+++ src/components/ExportCSV/ExportCSV.js
@@ -3,7 +3,7 @@ import { CSVLink, CSVDownload } from "react-csv";
 import "./ExportCSV.css";
 function ExportCSV(props) {
   return (
-    <button disabled={props.canDisable} className="btn btn-primary col-2 mb-1">
+    <button disabled={props.canDisable} className="btn btn-primary col-2 mb-1 report">
       <CSVLink data={props.data} filename="expense_report">
         <span
           class="d-inline-block"
